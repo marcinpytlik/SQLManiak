4338540b03a97be6f167b13372bb0e99dc7a5a21
Marcin Pytlik <marcin.pytlik@pracowniait.com>
2026-03-28 23:46:02 +0100

    Sprint 2 SqlAnomalyWatcher kolektor metryk Finish

--- ML/SqlAnomalyWatcher/sql/temp.sql
+++ ML/SqlAnomalyWatcher/sql/temp.sql
@@ -2,18 +2,23 @@ USE SqlAnomalyWatcherDb;
 GO
 SELECT COUNT(*) AS SnapshotCount
 FROM dbo.TelemetrySnapshot;
-python -m src.collector.collector
-Zapisano snapshot:
-CaptureTime: 2026-03-28 23:26:15.252760
-ServerName: deweloper
-InstanceName: MSSQLSERVER
-CpuPercent: 0.00
-BatchRequestsPerSec: 12726
-UserConnections: 12
-BlockedSessions: 0
-DeadlocksPerMin: 0
-AvgQueryDurationMs: 68.59721568
-AvgLogicalReads: 6607.882352
-TempdbUsedMb: 8.0000000
-SignalWaitTimeMs: 183728374
-PageLifeExpectancy: 822
+
+USE SqlAnomalyWatcherDb;
+GO
+
+SELECT TOP (20)
+    Id,
+    CaptureTime,
+    CpuPercent,
+    BatchRequestsPerSec,
+    UserConnections,
+    BlockedSessions,
+    DeadlocksPerMin,
+    AvgQueryDurationMs,
+    AvgLogicalReads,
+    TempdbUsedMb,
+    SignalWaitTimeMs,
+    PageLifeExpectancy
+FROM dbo.TelemetrySnapshot
+ORDER BY Id DESC;
+GO
